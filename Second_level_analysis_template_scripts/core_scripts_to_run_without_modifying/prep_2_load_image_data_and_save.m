5fc75abbabe4b6425808d02ba22f1b8f807b4099
lukasvo76 <60989355+lukasvo76@users.noreply.github.com>
2021-03-01 17:02:37 -0500

    adapted to z-score images, improved structure, removed inconsistencies, added documentation

--- Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/prep_2_load_image_data_and_save.m
+++ Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/prep_2_load_image_data_and_save.m
@@ -18,25 +18,30 @@
 options_needed = {'dofullplot', 'omit_histograms' 'dozipimages'};  % Options we are looking for. Set in a2_set_default_options
 options_exist = cellfun(@exist, options_needed); 
 
-option_default_values = {true false true};          % defaults if we cannot find info in a2_set_default_options at all 
+option_default_values = {true false false};          % defaults if we cannot find info in a2_set_default_options at all; lukasvo76: changed the default for zipping images
 
 plugin_get_options_for_analysis_script
 
 
 %% Prep and check image names
-% -------------------------------------------------------------------
+% -------------------------------------------------------------------------
 
 clear imgs cimgs
 
 for i = 1:length(DAT.conditions)
     
-    % printhdr(sprintf('Raw data, condition %3.0f, %s', i, DAT.conditions{i}));
+    printhdr(sprintf('Raw data, condition %3.0f, %s', i, DAT.conditions{i}));
     
     % This will vary based on your naming conventions
     % This version assumes that FOLDERS have names of CONDITIONS and images
     % are in each folder
+    %
+    % @lukasvo76: LaBGAS convention is to have subfolders by subject
+    % throughout analysis, so we leave DAT.subfolders blank in prep_1, and
+    % I adapted the else part of the if loop below to our conventional
+    % folder structure
     
-    if ~isempty(DAT.subfolders) && ~isempty(DAT.subfolders{1})  % if we have subfolders
+    if ~isempty(DAT.subfolders) && ~isempty(DAT.subfolders{i})  % if we have subfolders
         
         str = fullfile(datadir, DAT.subfolders{i}, DAT.functional_wildcard{i});
         
@@ -44,23 +49,26 @@ for i = 1:length(DAT.conditions)
         % note, Matlab's gunzip() does not remove .gz images, so use eval( ) version.
         % note, replace spaces with '\ ' 
         
-%         try eval(['!gunzip ' strrep(str, ' ', '\ ') '.gz']), catch, end     % gunzip([str '.gz'])
-%         cimgs{i} = filenames(str, 'absolute');
+        try eval(['!gunzip ' strrep(str, ' ', '\ ') '.gz']), catch, end     % gunzip([str '.gz'])
+        cimgs{i} = filenames(str, 'absolute');
         
         cimgs{i} = plugin_unzip_images_if_needed(str);
         
     else
         
-        str = fullfile(datadir, DAT.functional_wildcard{i});
+        str = spm_select('ExtFPListRec',datadir, DAT.functional_wildcard{i}, Inf); % lukasvo76: spm_select uses regular expressions as filter . is wildcard, not *!
         
-%         % Unzip if needed
-%         unzipstr = ['!gunzip ' strrep(str, ' ', '\ ') '.gz'];
+%         % Unzip if needed - not needed in LaBGAS case since we typically do
+%         not have zipped con images, although that could be implemented
 %         
-%         try eval(unzipstr), catch, end
-%         
+%         try eval(['!gunzip ' strrep(str, ' ', '\ ') '.gz']), catch, end
 %         cimgs{i} = filenames(str, 'absolute');
         
-        cimgs{i} = plugin_unzip_images_if_needed(str);
+        cimgs{i} = cellstr(str);
+        
+        for j = 1:size(cimgs{i},1)
+            cimgs{i}{j} = cimgs{i}{j}(1,1:end-2); % lukasvo76: gets rid of the ',1' added by spm_select at the end of the filename (first volume, but con images only have one volume)
+        end
         
     end
     
@@ -68,14 +76,13 @@ for i = 1:length(DAT.conditions)
     if isempty(cimgs{i}), fprintf('Looking in: %s\n', str), error('CANNOT FIND IMAGES. Check path names and wildcards.'); end
     cimgs{i} = cellfun(@check_valid_imagename, cimgs{i}, repmat({1}, size(cimgs{i}, 1), 1), 'UniformOutput', false);
     
-    DAT.imgs{i} = char(cimgs{i}{:});
-    
-    filename_string{i} = strrep(str, ' ', '\ ');  % used below for zipping images
+    DAT.imgs{i} = cimgs{i};
+
 end
 
 
 %% Load full objects
-% -------------------------------------------------------------------
+% -------------------------------------------------------------------------
 
 % Determine whether we want to sample to the mask (2 x 2 x 2 mm) or native
 % space, whichever is more space-efficient
@@ -91,54 +98,49 @@ else
 
 end
 
+printhdr('LOADING IMAGES INTO FMRI_DATA_ST OBJECTS');
+
 for i = 1:length(DAT.conditions)
     
     printhdr(sprintf('Loading images: condition %3.0f, %s', i, DAT.conditions{i}));
+    printstr(dashes);
     
     % If images are less than 2 mm res, sample in native space:
-    %DATA_OBJ{i} = fmri_data(DAT.imgs{i});
+    DATA_OBJ{i} = fmri_data_st(DAT.imgs{i}); % @lukasvo76: changed to @bogpetre's improved data_st object class
     
     % If images are very large/high-res, you may want to sample to the mask space instead:
-    DATA_OBJ{i} = fmri_data(DAT.imgs{i}, which('brainmask.nii'), sample_type_string, 'noverbose');
+%     DATA_OBJ{i} = fmri_data_st(DAT.imgs{i}, which('brainmask.nii'), sample_type_string, 'noverbose');
     
     % make sure we are using right variable types (space-saving)
     % this is new and could be a source of errors - beta testing!
     DATA_OBJ{i} = enforce_variable_types(DATA_OBJ{i});
      
     if dozipimages
-        % zip original files to save space (we are done using them now).
-        % try eval(['!gzip ' filename_string{i}]), catch, end
-
-        cmdstr = ['gzip -f ' filename_string{i}];
-        [status, cmdout] = system(cmdstr);  % cmdout is list of files; don't use, just for reference
-        
-        if status == 0
-            
-            disp('Zipped images into .gz successfully.');
-        else
-            disp('Failed to zip images into .gz.');
-        end
-        
+        % zip original files to save space and delete the unzipped images (we are done using them now).
+        for j=1:size(DAT.imgs{i},1)
+            gzip(DAT.imgs{i}{j});
+            delete(DAT.imgs{i}{j});
+        end 
     end
     
     % QUALITY CONTROL METRICS
-    % -------------------------------------------------------------------
+    % ---------------------------------------------------------------------
 
-    printstr('QC metrics');
+    printhdr(sprintf('QC metrics for images: condition %3.0f, %s', i, DAT.conditions{i}));
     printstr(dashes);
     
     [group_metrics individual_metrics values gwcsf gwcsfmean gwcsfl2norm] = qc_metrics_second_level(DATA_OBJ{i});
     
     DAT.quality_metrics_by_condition{i} = group_metrics;
+    DAT.gray_white_csf{i} = values;
     
     disp('Saving quality control metrics in DAT.quality_metrics_by_condition');
     disp('Saving gray, white, CSF means in DAT.gray_white_csf');
     
-    DAT.gray_white_csf{i} = values;
     drawnow; snapnow
     
     % optional: plot
-    % -------------------------------------------------------------------
+    % ---------------------------------------------------------------------
     
     if dofullplot
         fprintf('%s\nPlot of images: %s\n%s\n', dashes, DAT.functional_wildcard{i}, dashes);
@@ -168,51 +170,75 @@ for i = 1:length(DAT.conditions)
     drawnow, snapnow
 end
 
-%% CSF REMOVAL AND RESCALING
-printhdr('CSF REMOVAL AND RESCALING');
 
-% scaling by CSF values
-% ----------------------------------------------------------------
-% Note: Do not train cross-validated models on these scaled objects because
-% they perform operations using group information to transform individual
-% image values.
+%% Z-score images
+% -------------------------------------------------------------------------
 
-DATA_CAT = cat(DATA_OBJ{:});
+printhdr('Z-SCORING IMAGES');
 
-clear sz
-
-for i = 1:size(DATA_OBJ, 2), sz(1, i) = size(DATA_OBJ{i}.dat, 2); end
-DATA_CAT.images_per_session = sz;
-DATA_CAT.removed_images = 0;
-
-%DATA_CAT = preprocess(DATA_CAT, 'remove_csf');
-%DATA_CAT = preprocess(DATA_CAT, 'rescale_by_csf');
-%DATA_CAT = preprocess(DATA_CAT, 'divide_by_csf_l2norm');
-
-DATA_CAT = rescale(DATA_CAT, 'l2norm_images');     % scaling sensitive to mean and variance.
-
-DATA_CAT = preprocess(DATA_CAT, 'windsorize'); % entire data matrix
-
-if length(DAT.conditions) > 1
-    DATA_OBJsc = split(DATA_CAT);
-else
-    DATA_OBJsc = {DATA_CAT};
-end
-
-% Enforce variable types in objects to save space
-for i = 1:length(DATA_OBJsc), DATA_OBJsc{i} = enforce_variable_types(DATA_OBJsc{i}); end
-
-if dofullplot
-    disp('AFTER WINDSORIZING AND RESCALING BY L2NORM'); %'ADJUSTING FOR WM/CSF');
+for i=1:length(DAT.conditions)
     
-    plot(DATA_CAT); drawnow; snapnow
+    % Z-scoring
+    % ---------------------------------------------------------------------
     
-end
+    printhdr(sprintf('Z-scoring images: condition %3.0f, %s', i, DAT.conditions{i}));
+    printstr(dashes);
 
-clear DATA_CAT
+    DATA_OBJsc{i} = rescale(DATA_OBJ{i}, 'zscoreimages');
+
+    DATA_OBJsc{i} = enforce_variable_types(DATA_OBJsc{i});
+
+    % QUALITY CONTROL METRICS
+    % ---------------------------------------------------------------------
+
+    printhdr(sprintf('QC metrics for z-scored images: condition %3.0f, %s', i, DAT.conditions{i}));
+    printstr(dashes);
+    
+    [group_metrics individual_metrics values gwcsf gwcsfmean gwcsfl2norm] = qc_metrics_second_level(DATA_OBJsc{i});
+    
+    DAT.sc_quality_metrics_by_condition{i} = group_metrics;
+    DAT.sc_gray_white_csf{i} = values;
+    
+    disp('Saving quality control metrics in DAT.sc_quality_metrics_by_condition');
+    disp('Saving gray, white, CSF means in DAT.sc_gray_white_csf');
+    
+    drawnow; snapnow
+    
+    % optional: plot
+    % ---------------------------------------------------------------------
+    
+    if dofullplot
+        fprintf('%s\nPlot of z-scored images: %s\n%s\n', dashes, DAT.functional_wildcard{i}, dashes);
+        disp(DATA_OBJsc{i}.fullpath)
+        
+        plot(DATA_OBJsc{i}); drawnow; snapnow
+        
+        if ~omit_histograms
+            
+            hist_han = histogram(DATA_OBJsc{i}, 'byimage', 'singleaxis');
+            title([DAT.conditions{i} ' histograms for each image']);
+            drawnow; snapnow
+            
+            hist_han = histogram(DATA_OBJsc{i}, 'byimage', 'by_tissue_type');
+            drawnow; snapnow
+            
+        end
+        
+    end
+    
+    % derived measures
+    
+    DATA_OBJsc{i} = remove_empty(DATA_OBJsc{i});
+    DAT.sc_globalmeans{i} = mean(DATA_OBJsc{i}.dat)';
+    DAT.sc_globalstd{i} = std(DATA_OBJsc{i}.dat)';
+    
+    drawnow, snapnow
+
+end
 
 
 %% SAVE
+% -------------------------------------------------------------------------
 
 printhdr('Save results');
 
