c39cb9b32ea7935ddbcd659f8efa5b84dfb9138f
lukasvo76 <60989355+lukasvo76@users.noreply.github.com>
2021-03-10 17:02:01 -0500

    changed holdout sets from LOOCV to 5-fold cross-validation balancing over groups

--- Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/prep_3d_run_SVM_betweenperson_contrasts.m
+++ Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/prep_3d_run_SVM_betweenperson_contrasts.m
@@ -4,15 +4,20 @@
 % each image.
 % --------------------------------------------------------------------
 
-% USER OPTIONS
 
-% Now set in a2_set_default_options
-if ~exist('dosavesvmstats', 'var') || ~exist('dobootstrap', 'var') || ~exist('boot_n', 'var')
-    a2_set_default_options;
-end
+%% Now set in a2_set_default_options
+%--------------------------------------------------------------------------
+options_needed = {'dosavesvmstats', 'dobootstrap', 'boot_n','myscaling_svm_between'};  % Options we are looking for. Set in a2_set_default_options
+options_exist = cellfun(@exist, options_needed); 
 
-% Check for required DAT fields. Skip analysis and print warnings if missing.
+option_default_values = {true false 1000 'raw'};          % defaults if we cannot find info in a2_set_default_options at all 
+
+plugin_get_options_for_analysis_script
+
+
+%% Check for required DAT fields. Skip analysis and print warnings if missing.
 % ---------------------------------------------------------------------
+
 % List required fields in DAT, in cell array:
 required_fields = {'BETWEENPERSON', 'contrastnames', 'contrasts' 'contrastcolors'};
 
@@ -26,28 +31,28 @@ if isempty(spath)
     disp('Warning: spider toolbox not found on path; prediction may break')
 end
 
-myscaling = 'raw';          % 'raw' or 'scaled'
-
 if dobootstrap, svmtime = tic; end
 
-% Initialize fmridisplay slice display if needed, or clear existing display
+
+%% Get mask
+% -------------------------------------------------------------------------
+
+if exist('maskname_svm', 'var') && ~isempty(maskname_svm)
+    
+    disp('Masking data')
+    svmmask = fmri_data(maskname_svm, 'noverbose');
+    
+else
+    
+    disp('No mask found; using full original image data');
+
+end
+
+
+%% Run between-person SVM for each contrast
 % --------------------------------------------------------------------
-
-% Specify which montage to add title to. This is fixed for a given slice display
-% whmontage = 5; 
-% plugin_check_or_create_slice_display; % script, checks for o2 and uses whmontage
-
-% --------------------------------------------------------------------
-
 
 printhdr('Cross-validated SVM to discriminate between-person contrasts');
-
-
-% --------------------------------------------------------------------
-%
-% Run between-person SVM for each contrast
-%
-% --------------------------------------------------------------------
 
 kc = size(DAT.contrasts, 1);
 
@@ -56,7 +61,8 @@ svm_stats_results = cell(1, kc);
 for c = 1:kc
     
     mygroupnamefield = 'contrasts';  % 'conditions' or 'contrasts'
-    [group, groupnames, groupcolors] = plugin_get_group_names_colors(DAT, mygroupnamefield, c);
+    [group, ~, ~] = plugin_get_group_names_colors(DAT, mygroupnamefield, c);
+    group = group(:,1); % @lukasvo76: added to correctly handle the situation where you added other covariates in addition to group in DAT.BETWEENPERSON.mygroupnamefield{c}
     outcome_value = group;
     
     if isempty(group)
@@ -64,19 +70,16 @@ for c = 1:kc
         continue
     end
     
-    % b. Define holdout sets: Leave one subject out
-    %    Assume that subjects are in same position in each input file
+    % Define holdout sets: 5-folds, balanced over groups
+    % @lukasvo76: changed from the original LOOCV code
+    % Assume that subjects are in same position in each input file!
     % --------------------------------------------------------------------
-
-    % strategy here is to keep training set size proportional to original
-    % sample proportions.  leave out pairs, 1 person from each group.
-    holdout_set = xval_select_holdout_set_categoricalcovs(group);
     
-    % Transform into integer vector of which holdout set for each
-    % observation
-    hs = cat(2, holdout_set{:});
-    [holdout_set, ~] = find(hs');
-    
+    nfolds = 5; % @lukasvo76: hardcoded for now, but can easily be adapted
+    cvpart = cvpartition(group,'KFOLD',nfolds);
+    [I,J] = find([cvpart.test(1),cvpart.test(2), cvpart.test(3), cvpart.test(4), cvpart.test(5)]);
+    holdout_set = sortrows([I,J]);
+    holdout_set = holdout_set(:,2);
     
     printstr(DAT.contrastnames{c});
     printstr(dashes)
@@ -87,22 +90,40 @@ for c = 1:kc
     % Select data for this contrast
     % --------------------------------------------------------------------
     
-    switch myscaling
+    switch myscaling_svm_between
         case 'raw'
             printstr('Raw (unscaled) images used in between-person SVM');
+            scaling_string = 'no_scaling';
             cat_obj = DATA_OBJ_CON{c};
             
         case 'scaled'
-            printstr('Scaled images used in between-person SVM');
-            cat_obj = DATA_OBJ_CON{c};
+            printstr('Z-scored images used in between-person SVM');
+            scaling_string = 'scaling_z_score_conditions';
+            cat_obj = DATA_OBJ_CONsc{c};
+            
+        case 'scaled_contrasts'
+            printstr('l2norm scaled contrast images used in between-person GLM');
+            scaling_string = 'scaling_l2norm_contrasts';
+            cat_obj = DATA_OBJ_CONscc{c};
             
         otherwise
-            error('myscaling must be ''raw'' or ''scaled''');
+            error('myscaling must be ''raw'' or ''scaled'' or ''scaled_contrasts''');
     end
     
-  
+    % Apply mask if specified in a2_ script
+    %----------------------------------------------------------------------
+    if exist('svmmask', 'var')
+        
+        disp('Masking data')
+        cat_obj = apply_mask(cat_obj, svmmask);
+        
+    else
+        
+        disp('No mask found; using full existing image data');
+        
+    end
     
-    % a. Format and attach outcomes: 1, -1 for pos/neg contrast values
+    % Format and attach outcomes: 1, -1 for pos/neg contrast values
     % --------------------------------------------------------------------
  
     cat_obj.Y = outcome_value;
@@ -120,11 +141,6 @@ for c = 1:kc
     
     % Run prediction model
     % --------------------------------------------------------------------
-    
-    % [cverr, stats, optout] = predict(cat_obj, 'algorithm_name', 'cv_svm', 'nfolds', holdout_set, 'error_type', 'mcr');
-    
-        % Run prediction model
-    % --------------------------------------------------------------------
     if dobootstrap
         [cverr, stats, optout] = predict(cat_obj, 'algorithm_name', 'cv_svm', 'nfolds', holdout_set, 'bootsamples', boot_n, 'error_type', 'mcr', parallelstr);
         % Threshold, if possible - can re-threshold later with threshold() method
@@ -134,56 +150,29 @@ for c = 1:kc
         [cverr, stats, optout] = predict(cat_obj, 'algorithm_name', 'cv_svm', 'nfolds', holdout_set, 'error_type', 'mcr', parallelstr);
     end
     
-    % Summarize output and create ROC plot
-    % --------------------------------------------------------------------
-    figtitle = sprintf('SVM ROC %s', DAT.contrastnames{c});
-    create_figure(figtitle);
-    
-    disp(' ');
-    printstr(['Results: ' DAT.contrastnames{c}]); printstr(dashes);
-    
-    ROC = roc_plot(stats.dist_from_hyperplane_xval, logical(cat_obj.Y > 0), 'color', DAT.contrastcolors{c}, 'Optimal balanced error rate');
-    
-    plugin_save_figure;
-    close
-    
-    % Effect size, cross-validated
-    dfun2 = @(x, Y) (mean(x(Y > 0)) - mean(x(Y < 0))) ./ sqrt(var(x(Y > 0)) + var(x(Y < 0))); % check this.
-    
-    d = dfun2(stats.dist_from_hyperplane_xval, stats.Y);
-    fprintf('Effect size, cross-val: d = %3.2f\n\n', d);
-    
     % Save stats objects for results later
     % --------------------------------------------------------------------
     
-    stats.weight_obj = enforce_variable_types(stats.weight_obj);
+%     stats.weight_obj = enforce_variable_types(stats.weight_obj);
     svm_stats_results{c} = stats;
+    
+    if exist('svmmask', 'var')
+    
+        svm_stats_results{c}.mask = svmmask;
+        svm_stats_results{c}.maskname = maskname_svm;
+    
+    end
         
-    if dobootstrap, disp('Cumulative run time:'), toc(svmtime); end
-    
-    
-%     % Plot the SVM map
-%     % --------------------------------------------------------------------
-%     o2 = removeblobs(o2);
-%     o2 = addblobs(o2, region(stats.weight_obj), 'trans');
-%     
-%     axes(o2.montage{whmontage}.axis_handles(5));
-%     title(DAT.contrastnames{c}, 'FontSize', 18)
-%     
-%     printstr(DAT.contrastnames{c}); printstr(dashes);
-% 
-%     figtitle = sprintf('SVM weight map nothresh %s', DAT.contrastnames{c});
-%     plugin_save_figure;
-%        
-%     o2 = removeblobs(o2);
+    if dobootstrap, disp('Cumulative run time:'), toc(svmtime); end  
     
 end  % between-person contrast
 
-% Save
+
+%% Save
 % --------------------------------------------------------------------
 if dosavesvmstats
     
-    savefilenamedata = fullfile(resultsdir, 'svm_stats_results_betweenperson_contrasts.mat');
+    savefilenamedata = fullfile(resultsdir,['svm_stats_results_betweenperson_contrasts_',scaling_string,'.mat']);
 
     save(savefilenamedata, 'svm_stats_results', '-v7.3');
     printhdr('Saved svm_stats_results for contrasts');
