686ee1b278d7c5e6be859c33aef83692799f9b83
lukasvo76 <60989355+lukasvo76@users.noreply.github.com>
2021-03-11 16:31:23 -0500

    created this new script from h2_group_differences_emosignatures.m to compare signature responses to conditions in addition to contrasts between groups

---
+++ Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/h2_group_differences_emosignatures_conditions.m
@@ -0,0 +1,91 @@
+% Runs a two-sample t-test for a set of signature responses, for each
+% condition.
+% Signatures, scaling, etc. are defined below.
+% created by @lukasvo76 from h2_group_differences_emosignatures.m, March 2021
+
+
+%% USER OPTIONS
+% -------------------------------------------------------------------------
+
+mysignature =   {'PINES', 'Rejection', 'VPS', 'VPS_nooccip'};
+scalenames =    {'raw'};                                % or scaled
+simnames =      {'dotproduct'};                         % or 'cosine_sim' 'dotproduct'
+mygroupnamefield = 'conditions';  % 'conditions' or 'contrasts'
+
+
+%% DEFINE GROUPS IN PREP_1b_PREP_BEHAVIORAL_DATA
+% -------------------------------------------------------------------------
+
+% There are two ways to define groups. The other is condition- and
+% contrast-specific.  These are entered in DAT.BETWEENPERSON.conditions and
+% DAT.BETWEENPERSON.contrasts, in cells.  1 -1 codes work best. 
+% These are set up in prep_1b_prep_behavioral_data.m
+% If they are missing, using DAT.BETWEENPERSON.group will be used as a
+% generic option.
+% -------------------------------------------------------------------------
+
+
+%% LOOP THROUGH SIGNATURES, TEST GROUP DIFFERENCE, CREATE ONE PLOT PER CONTRAST
+% -------------------------------------------------------------------------
+for s = 1:length(mysignature)
+    
+    % Get data
+    % -------------------------------------------------------------------------
+    conditiondata = table2array(DAT.SIG_conditions.(scalenames{1}).(simnames{1}).(mysignature{s}));
+    
+    kc = size(conditiondata, 2);
+    
+    % Plot
+    % -------------------------------------------------------------------------
+    printhdr(sprintf('%s responses: Scale = %s Metric = %s', mysignature{s}, scalenames{1}, simnames{1}));
+    
+    figtitle = sprintf('%s group diffs %s %s', mysignature{s}, scalenames{1}, simnames{1});
+    create_figure(figtitle, 1, kc);
+    
+    for i = 1:kc
+        
+        % Load group variable
+        [group, groupnames, groupcolors] = plugin_get_group_names_colors(DAT, mygroupnamefield, i);
+        
+        if isempty(group), continue, end % skip this condition/contrast - no groups
+        
+        if size(group, 2) > 1            % we have multiple variables
+            disp('Warning: Group has > 1 column. Using first column only.')
+            group = group(:, 1);
+        end
+        
+        if length(unique(group)) > 2  % this is a continuous variable
+            disp('Binarizing continuous grouping variable via median split.')
+            group = mediansplit(group);  
+        end
+        
+        % In case we forgot to assign colors or groupnames in prep 1b script
+        if length(groupcolors) < 2, groupcolors = seaborn_colors(2); end
+        if length(groupnames) < 2, groupnames = {'High' 'Low'}; end
+        
+        %y = {DAT.(myfield){i}(group > 0) DAT.(myfield){i}(group < 0)};
+        y = {conditiondata(group > 0, i) conditiondata(group < 0, i)};
+        
+        subplot(1, kc, i)
+        
+        printstr(' ');
+        printstr(sprintf('Group differences: %s, %s', mysignature{s}, DAT.conditions{i}));
+        printstr(dashes)
+        
+        barplot_columns(y, 'nofig', 'colors', groupcolors, 'names', groupnames);
+        
+        title(DAT.conditions{i})
+        xlabel('Group');
+        ylabel(sprintf('%s Response', mysignature{s}));
+        
+        printstr('Between-groups test:');
+        
+        [H,p,ci,stats] = ttest2_printout(y{1}, y{2});
+        
+        printstr(dashes)
+        
+    end % panels
+    
+    drawnow, snapnow
+    
+end % signature
