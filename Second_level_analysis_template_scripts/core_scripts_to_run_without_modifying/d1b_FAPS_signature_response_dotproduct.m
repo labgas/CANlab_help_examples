f9e24d378058973cc3a0771d736514d6c02ecea2
lukasvo76 <60989355+lukasvo76@users.noreply.github.com>
2021-04-15 12:41:38 -0400

    adapted script to plot FAPS rather than NPS response

---
+++ Second_level_analysis_template_scripts/core_scripts_to_run_without_modifying/d1b_FAPS_signature_response_dotproduct.m
@@ -0,0 +1,5 @@
+signatures_to_plot = {'FAPS'};  % NPS, VPS, SIIPS, etc., etc.
+myscaling = 'raw';                                       % 'raw' or 'scaled'
+mymetric = 'dotproduct';                                 % 'dotproduct' or 'cosine_sim'
+
+plugin_signature_condition_contrast_plot
